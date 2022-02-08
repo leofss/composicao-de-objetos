405ec0718ebfdaca2b7df271a2614ca023c67e82
leonardo fernandes <leonardo fernandes@LAPTOP-VL5MJIVC>
2022-02-08 15:56:54 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Worker.cs
@@ -0,0 +1,13 @@
+﻿using composicao_exercicio.Entities.Enums;
+
+
+namespace composicao_exercicio.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace composicao_exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
