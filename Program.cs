504b06102ef1e2078e2262cae45c23e6f2fa3b16
leonardo fernandes <leonardo fernandes@LAPTOP-VL5MJIVC>
2022-02-08 16:53:13 -0300

    Inserção da lógica necessária para adicionar contratos em um trabalhador

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using composicao_exercicio.Entities.Enums;
+using composicao_exercicio.Entities;
 
 namespace composicao_exercicio
 {
@@ -6,7 +8,39 @@ namespace composicao_exercicio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Departament name");
+            string deptName = Console.ReadLine();
+
+            Console.WriteLine("Enter worker data");
+            Console.Write("Name :");
+            string name = Console.ReadLine();
+            Console.Write("Enter level (Junior/mid level/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Enter base salary");
+            double baseSalary = double.Parse(Console.ReadLine());
+
+            Department dept = new Department(deptName);//instanciei objeto departamento
+
+            Worker worker = new Worker(name, level, baseSalary, dept);//inst objeto worker
+
+            Console.Write("How many contracts to this worker?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"Enter #{i} contract data: ");
+                Console.Write("Date ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Vlue per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+                Console.Write("Duration(hours) ");
+                int hours = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+                worker.AddContract(contract);
+
+            }
+
+
         }
     }
 }
